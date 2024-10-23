2cd6a60c377fe236dc9697e0df755aec8c33db5d
pavansailyros <pavansai@lyrostech.com>
2024-10-23 16:13:05 +0530

    chnages names

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)"
+      },
+
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+
+
+      fontSize: {
+        'custom-16': ['16px', '24px'],  // font-size of 16px with line-height of 24px
+      },
+
+
+      fontWeight: {
+        500: '500',  // font-weight of 500
+      },
+
+      
+    },
   },
   plugins: [],
 }
